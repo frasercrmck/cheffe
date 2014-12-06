b36b32927c7121d746f53e79cdf9439add9080b9
Fraser Cormack <frasercrmck@gmail.com>
2014-12-06 16:36:25 +0000

    Emit colour diagnostics if they're supported by the terminal

--- src/Utils/CheffeDiagnosticHandler.cpp
+++ src/Utils/CheffeDiagnosticHandler.cpp
@@ -1,6 +1,20 @@
 #include "CheffeDiagnosticHandler.h"
 
 #include <cassert>
+#include <map>
+
+#if !defined(_WIN32) && !defined(__WIN64) && defined(__unix__) ||              \
+    (defined(__APPLE__) && defined(__MACH__))
+#define CHEFFE_POSIX 1
+#else
+#define CHEFFE_POSIX 0
+#endif
+
+#if CHEFFE_POSIX
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#endif
 
 namespace cheffe
 {
@@ -10,19 +24,82 @@ std::ostream &CheffeDiagnosticHandler::errs()
   return std::cerr;
 }
 
+// A conservative method to determine whether to emit colour diagnostics:
+// * if it's a unix system
+// * if stderr is a TTY
+// * if $TERM is an xterm
+static bool shouldUseColour()
+{
+#ifndef CHEFFE_POSIX
+  // Be conversative: don't support colour
+  return false;
+#endif
+  const char *const TermVar = std::getenv("TERM");
+  const bool TerminalSupportsColour = !std::strcmp(TermVar, "xterm") ||
+                                      !std::strcmp(TermVar, "xterm-color") ||
+                                      !std::strcmp(TermVar, "xterm-256color");
+  const bool IsATTY = isatty(fileno(stderr)) != 0;
+  return IsATTY && TerminalSupportsColour;
+}
+
+enum class ColourKind
+{
+  RedBold,
+  YellowBold,
+  MagentaBold
+};
+
+const std::map<ColourKind, std::string> ColourCodes = {
+    {ColourKind::RedBold, "\x1b[31;1m"},
+    {ColourKind::YellowBold, "\x1b[33;1m"},
+    {ColourKind::MagentaBold, "\x1b[35;1m"}};
+
+struct ColourText
+{
+public:
+  ColourText(const std::string &Message, const ColourKind Colour)
+      : Message(Message)
+  {
+    assert(ColourCodes.find(Colour) != std::end(ColourCodes) &&
+           "Invalid Colour Code");
+    ColourOn = shouldUseColour() ? ColourCodes.find(Colour)->second : "";
+    ColourOff = shouldUseColour() ? "\x1b[0m" : "";
+  }
+
+private:
+  const std::string &Message;
+  std::string ColourOn;
+  std::string ColourOff;
+
+  friend std::ostream &operator<<(std::ostream &os, const ColourText &Text)
+  {
+    os << Text.ColourOn << Text.Message << Text.ColourOff;
+    return os;
+  }
+};
+
 void CheffeDiagnosticHandler::formatAndLogMessage(
     const std::string &Message, const SourceLocation SourceLoc,
     const DiagnosticKind Kind, const LineContext Context)
 {
   std::stringstream ss;
+  if (Kind == DiagnosticKind::Error)
+  {
+    ss << ColourText("Error", ColourKind::RedBold) << ": ";
+  }
+  else if (Kind == DiagnosticKind::Warning)
+  {
+    ss << ColourText("Warning", ColourKind::YellowBold) << ": ";
+  }
   ss << getFileAndLineNumberInfoAsString(SourceLoc.getLineNo(),
                                          SourceLoc.getColumnNo()) << std::endl;
   ss << Message << std::endl;
   if (Context == LineContext::WithContext)
   {
     ss << getLineAsString(SourceLoc.getLineNo()) << std::endl;
-    ss << getContextAsString(SourceLoc.getColumnNo(), SourceLoc.getLength())
-       << std::endl;
+    ss << ColourText(getContextAsString(SourceLoc.getColumnNo(),
+                                        SourceLoc.getLength()),
+                     ColourKind::MagentaBold) << std::endl;
   }
 
   switch (Kind)
