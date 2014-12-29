b83ea4d17e080fea02264b965fb93e85cd5293f1
Fraser Cormack <frasercrmck@gmail.com>
2014-12-29 20:20:43 +0000

    Add JIT helpers to query & check ingredients

--- src/JIT/CheffeJIT.cpp
+++ src/JIT/CheffeJIT.cpp
@@ -1,6 +1,7 @@
 #include "cheffe.h"
 #include "JIT/CheffeJIT.h"
 #include "IR/CheffeRecipeInfo.h"
+#include "IR/CheffeIngredient.h"
 #include "Utils/CheffeDebugUtils.h"
 
 #include <iostream>
@@ -9,6 +10,41 @@
 
 namespace cheffe
 {
+
+CheffeErrorCode
+CheffeJIT::getIngredientInfo(const std::shared_ptr<MethodOp> &MOp,
+                             std::shared_ptr<CheffeIngredient> &IngredientInfo,
+                             SourceLocation &IngredientLoc)
+{
+  auto Ingredient = std::static_pointer_cast<IngredientOp>(MOp);
+  if (!Ingredient->getIngredient())
+  {
+    Diagnostics->report(Ingredient->getSourceLoc(), DiagnosticKind::Error,
+                        LineContext::WithContext)
+        << "Trying to use an undefined ingredient";
+    return CheffeErrorCode::CHEFFE_ERROR;
+  }
+
+  IngredientLoc = Ingredient->getSourceLoc();
+  IngredientInfo = Ingredient->getIngredient();
+
+  return CheffeErrorCode::CHEFFE_SUCCESS;
+}
+
+bool CheffeJIT::checkIngredientHasValue(
+    const std::shared_ptr<CheffeIngredient> &Ingredient,
+    const SourceLocation IngredientLoc)
+{
+  if (Ingredient->HasValue)
+  {
+    return true;
+  }
+
+  Diagnostics->report(IngredientLoc, DiagnosticKind::Error,
+                      LineContext::WithContext)
+      << "Using ingredient '" << Ingredient->Name << "' without a value";
+  return false;
+}
 
 CheffeErrorCode CheffeJIT::executeRecipe()
 {
--- src/JIT/CheffeJIT.h
+++ src/JIT/CheffeJIT.h
@@ -3,6 +3,8 @@
 
 #include "cheffe.h"
 #include "CheffeCommon.h"
+#include "IR/CheffeIngredient.h"
+#include "IR/CheffeMethodStep.h"
 #include "IR/CheffeProgramInfo.h"
 #include "Utils/CheffeDiagnosticHandler.h"
 
@@ -23,6 +25,14 @@ public:
 private:
   std::unique_ptr<CheffeProgramInfo> ProgramInfo;
   std::shared_ptr<CheffeDiagnosticHandler> Diagnostics;
+
+  bool
+  checkIngredientHasValue(const std::shared_ptr<CheffeIngredient> &Ingredient,
+                          const SourceLocation IngredientLoc);
+  CheffeErrorCode
+  getIngredientInfo(const std::shared_ptr<MethodOp> &MOp,
+                    std::shared_ptr<CheffeIngredient> &IngredientInfo,
+                    SourceLocation &IngredientLoc);
 };
 
 } // end namespace cheffe
